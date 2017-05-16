0b24e29067eac2f3538168b9d157bc3521b25c71
gs-kodeit <gsanchez@kodeit.com.ve>
2017-05-16 15:12:03 -0400

    Agregar numero de fila a titulo de ver mas. Agregar observaciones a ver mas

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -30,7 +30,7 @@
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
-        			<h4 class="modal-title">Datos del registro</h4>
+        			<h4 class="modal-title" id="info">Datos del registro</h4>
 				</div>
 				
 				
@@ -46,6 +46,8 @@
 							<dd class="col-sm-9" id="person_occupation"></dd><br>
 							<dt class="col-sm-3">Empresa:</dt>
 							<dd class="col-sm-9" id="person_company"></dd>
+							<dt class="col-sm-3">Observaciones:</dt>
+							<dd class="col-sm-9" id="person_observations"></dd>
 						</dl>
 					</div>
 						
@@ -58,6 +60,8 @@
 							<dd class="col-sm-9" id="material_quantity"></dd><br>
 							<dt class="col-sm-3">Unidad</dt>
 							<dd class="col-sm-9" id="unit"></dd>
+							<dt class="col-sm-3">Observaciones:</dt>
+							<dd class="col-sm-9" id="material_observations"></dd>
 						</dl>
 					</div>				
 					
@@ -72,6 +76,8 @@
 							<dd class="col-sm-9" id="driver_name"></dd><br>
 							<dt class="col-sm-3">Cédula:</dt>
 							<dd class="col-sm-9" id="driver_id"></dd>
+							<dt class="col-sm-3">Observaciones:</dt>
+							<dd class="col-sm-9" id="vehicle_observations"></dd>
 						</dl>
 					</div>
 					
@@ -104,7 +110,7 @@
 			</thead>
 			<tbody>
 				@foreach($entries as $index => $entrie)
-					<tr id="entry{{ $entrie->id }}">
+					<tr id="{{ $index + 1 }}">
 						<td>{{ $index + 1 }}</td>
 						<td>{{ $entrie->date->format('d/m/Y')}}</td>
 						<td>{{ $entrie->time }}</td>
@@ -169,6 +175,9 @@
 		$('#myModalInfo').on('show.bs.modal', function (event) {
         	var button = $(event.relatedTarget) // Button that triggered the modal
         	var entry_id = button.data('id')
+        	var tr = button.closest('tr').attr('id')
+        	$('h4[id="info').text('Datos del registro N° ' + tr)
+        	
 
         	$.get('/entry/getEntry/' + entry_id, function(response){      
 
@@ -176,19 +185,22 @@
 	          	$('dd[id="person_id').text(response.person_id)
 	          	$('dd[id="person_occupation').text(response.person_occupation)
 	          	$('dd[id="person_company').text(response.person_company)  
+	          	$('dd[id="person_observations').text(response.person_observations)  
 
 	          	$('dd[id="material_type').text(response.material_type)
 	          	$('dd[id="material_quantity').text(response.material_quantity)
 	          	var unit_id = response.unit_id
 	          	$.get('/unit/getUnit/' + unit_id, function(unit){
 	          		console.log(unit)
-	          		$('dd[id="unit').text(unit.code)
+	          		$('dd[id="unit').text(unit.code + ' - ' + unit.name)
 	          	})
+	          	$('dd[id="material_observations').text(response.material_observations)
 
 	          	$('dd[id="vehicle').text(response.vehicle)
 	          	$('dd[id="vehicle_plate').text(response.vehicle_plate)
 	          	$('dd[id="driver_name').text(response.driver_name)
 	          	$('dd[id="driver_id').text(response.driver_id)
+	          	$('dd[id="vehicle_observations').text(response.vehicle_observations)
 
         		$.get('/category/getCategory/' + response.categorie_id, function(category){
         			if(category.person == 1){
